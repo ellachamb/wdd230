88ba10d71284dbc8640c1c2fcacadbb9b74c5237
ellachamb <ellachamb@gmail.com>
2023-01-26 11:00:34 -0700

    changing

--- chamber/scripts/site.js
+++ chamber/scripts/site.js
@@ -1,9 +1,3 @@
-
-const today = new Date(); 
-document.querySelector("#currentYear").textContent = today.getFullYear(); 
-
-document.querySelector("#lastModified").textContent = document.lastModified; 
-
 function toggleMenu() {
     document.querySelector('#navbar').classList.toggle('menu-active'); 
     document.querySelector('#menu-close').classList.toggle('menu-active'); 
