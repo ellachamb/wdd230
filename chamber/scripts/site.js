5a2af1936477e2bbeaa8ab92cb05d105a83eb57d
ellachamb <ellachamb@gmail.com>
2023-01-27 18:33:17 -0700

    plans

--- chamber/scripts/site.js
+++ chamber/scripts/site.js
@@ -5,3 +5,11 @@ function toggleMenu() {
 }
 
 document.querySelector('#ham-menu').addEventListener('click', toggleMenu); 
+
+const today = new Date(); 
+document.querySelector("#currentYear").textContent = today.getFullYear(); 
+
+document.querySelector("#lastModified").textContent = document.lastModified; 
+
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format( new Date() );
+document.querySelector("#current").textContent = fulldate; 
