b637a1919126f630f49920f14315496867b9ccb3
ellachamb <ellachamb@gmail.com>
2023-02-18 21:31:06 -0700

    lazy

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,4 +1,4 @@
 var messagedate = new Date();
-if (messagedate.getDay()==2 || messagedate.getDay()==4) {
-    document.querySelector("meet-greet").classList.add('active'); 
+if (messagedate.getDay()==1 || messagedate.getDay()==2) {
+    document.querySelector("#meet-greet").classList.add('active'); 
 }
